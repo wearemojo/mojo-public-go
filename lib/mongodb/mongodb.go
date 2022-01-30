a3aca0c098ffcd5719d69610803689d90c066f1d
James Billingham <git@jamesbillingham.com>
2022-01-30 10:44:57 +0000

    add `mongodb` lib

---
+++ lib/mongodb/mongodb.go
@@ -0,0 +1,68 @@
+package mongodb
+
+import (
+	"context"
+	"fmt"
+	"io/fs"
+
+	"github.com/cuvva/cuvva-public-go/lib/config"
+	"github.com/cuvva/cuvva-public-go/lib/db/mongodb"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+type MongoDB struct {
+	uriSecretID string
+}
+
+func New(uriSecretID string) *MongoDB {
+	return &MongoDB{uriSecretID: uriSecretID}
+}
+
+func (m *MongoDB) Connect(ctx context.Context, schemaFS fs.FS, collectionNames []string) (*mongodb.Database, error) {
+	uri, err := secrets.Get(ctx, m.uriSecretID)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO: handle reconnection in some way?
+	// in case the credentials change since the initial connection
+	db, err := config.MongoDB{URI: uri}.Connect()
+	if err != nil {
+		return nil, err
+	}
+
+	err = setupCollections(ctx, db, collectionNames)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.SetupSchemas(ctx, schemaFS, collectionNames)
+	if err != nil {
+		return nil, fmt.Errorf("schemas: %w", err)
+	}
+
+	return db, nil
+}
+
+func setupCollections(ctx context.Context, db *mongodb.Database, names []string) error {
+	existingNames, err := db.ListCollectionNames(ctx, bson.M{})
+	if err != nil {
+		return err
+	}
+
+outer:
+	for _, name := range names {
+		for _, existingName := range existingNames {
+			if name == existingName {
+				continue outer
+			}
+		}
+
+		err = db.CreateCollection(ctx, name)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
