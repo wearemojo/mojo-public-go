66e05c0a99ece3bc548207d677e2cb2a7583c964
James Billingham <git@jamesbillingham.com>
2022-02-10 08:34:31 +0000

    fix silly naming mistake on secret wrapper

--- lib/secret/wrapper.go
+++ lib/secret/wrapper.go
@@ -8,7 +8,7 @@ type Wrapper struct {
 	secretID string
 }
 
-func New(ctx context.Context, secretID string) (w *Wrapper, err error) {
+func NewWrapper(ctx context.Context, secretID string) (w *Wrapper, err error) {
 	w = &Wrapper{secretID: secretID}
 	_, err = w.Get(ctx)
 	return
