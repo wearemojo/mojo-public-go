60acb10d9f83709c96d6f395e67ad60b0b338796
James Billingham <git@jamesbillingham.com>
2022-04-06 10:58:42 +0100

    add `cryptorand`

---
+++ lib/cryptorand/cryptorand.go
@@ -0,0 +1,25 @@
+package cryptorand
+
+import (
+	"crypto/rand"
+	"encoding/binary"
+	mathrand "math/rand"
+)
+
+type source struct{}
+
+func NewSource() mathrand.Source {
+	return source{}
+}
+
+func (_ source) Seed(_ int64) {}
+
+func (_ source) Int63() int64 {
+	var b [8]byte
+	if _, err := rand.Read(b[:]); err != nil {
+		panic(err)
+	}
+
+	// mask off sign bit to ensure positive number
+	return int64(binary.LittleEndian.Uint64(b[:]) & (1<<63 - 1))
+}
