45cdff019ecfd54d1f2c4c6ff086bedcb40f002a
James Billingham <git@jamesbillingham.com>
2022-04-11 12:02:55 +0100

    merrclog: add package for simple logging of merr errors

---
+++ lib/merrclog/log.go
@@ -0,0 +1,37 @@
+package merrlog
+
+import (
+	"context"
+
+	"github.com/cuvva/cuvva-public-go/lib/clog"
+	"github.com/sirupsen/logrus"
+	"github.com/wearemojo/mojo-public-go/lib/merr"
+)
+
+func Info(ctx context.Context, err merr.EInterface) {
+	log(ctx, logrus.InfoLevel, err)
+}
+
+func Warn(ctx context.Context, err merr.EInterface) {
+	log(ctx, logrus.WarnLevel, err)
+}
+
+func Error(ctx context.Context, err merr.EInterface) {
+	log(ctx, logrus.ErrorLevel, err)
+}
+
+func log(ctx context.Context, level logrus.Level, err merr.EInterface) {
+	if err == nil {
+		return
+	}
+
+	merr := err.GetConcrete()
+
+	// logrus runs `.String()` on anything implementing `error`
+	// so to get proper JSON, we need to copy the fields instead
+	fields := merr.Fields()
+
+	clog.Get(ctx).
+		WithField(logrus.ErrorKey, fields).
+		Log(level, merr.Code)
+}
