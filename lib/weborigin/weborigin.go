8e44fd50e193a5831804193e8fa4595e2587c8ed
Laura Miller <git@duffle.one>
2022-04-20 14:24:13 +0100

    weborigin pkg (#26)
    * baseurl pkg
    
    * weborigin
    
    * linting & comments
    
    * varname
    
    * it doesn’t parse, it gets

---
+++ lib/weborigin/weborigin.go
@@ -0,0 +1,34 @@
+package weborigin
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/wearemojo/mojo-public-go/lib/merr"
+)
+
+// ParseWebOrigin takes a URL and returns a string to use to match web origins.
+// The definition of the web origin is quite complex so you should refer to the
+// test cases of this lib to see how it works.
+func GetWebOrigin(url *url.URL) (string, error) {
+	if url.Scheme != "http" && url.Scheme != "https" {
+		return "", merr.New("invalid_scheme", nil)
+	}
+
+	if url.Hostname() == "" {
+		return "", merr.New("invalid_hostname", nil)
+	}
+
+	webOrigin := fmt.Sprintf("%s://%s", url.Scheme, url.Hostname())
+
+	return webOrigin, nil
+}
+
+func MustGetWebOrigin(in *url.URL) string {
+	webOrigin, err := GetWebOrigin(in)
+	if err != nil {
+		panic(err)
+	}
+
+	return webOrigin
+}
---
+++ lib/weborigin/weborigin_test.go
@@ -0,0 +1,84 @@
+package weborigin
+
+import (
+	"net/url"
+	"testing"
+
+	"github.com/wearemojo/mojo-public-go/lib/merr"
+)
+
+var tests = []struct {
+	Name     string
+	URL      string
+	Expected string
+	Err      merr.Code
+}{
+	{
+		Name:     "with trailing slash",
+		URL:      "https://example.com/",
+		Expected: "https://example.com",
+	},
+	{
+		Name:     "with path",
+		URL:      "https://example.com/foo/bar",
+		Expected: "https://example.com",
+	},
+	{
+		Name:     "with path and port",
+		URL:      "https://google.com:8443/bar",
+		Expected: "https://google.com",
+	},
+	{
+		Name:     "with http and path",
+		URL:      "http://google.com/foo",
+		Expected: "http://google.com",
+	},
+	{
+		Name:     "it is already perfect",
+		URL:      "https://app.mojo.so",
+		Expected: "https://app.mojo.so",
+	},
+	{
+		Name:     "gibberish",
+		URL:      "alskdjsdkghkjdfg",
+		Expected: "",
+		Err:      merr.Code("invalid_scheme"),
+	},
+	{
+		Name:     "missing scheme",
+		URL:      "google.com",
+		Expected: "",
+		Err:      merr.Code("invalid_scheme"),
+	},
+	{
+		Name:     "missing hostname",
+		URL:      "https://",
+		Expected: "",
+		Err:      merr.Code("invalid_hostname"),
+	},
+}
+
+func TestGetWebOrigin(t *testing.T) {
+	for _, test := range tests {
+		test := test
+
+		t.Run(test.Name, func(t *testing.T) {
+			url, err := url.Parse(test.URL)
+			if err != nil {
+				t.Errorf("unexpected error: %s", err)
+			}
+
+			actual, err := GetWebOrigin(url)
+			if err != nil && test.Err != "" {
+				if merr.IsCode(err, test.Err) {
+					return
+				}
+
+				t.Errorf("unexpected error: %v", err)
+			}
+			if actual != test.Expected {
+				t.Errorf("%s, expected %s, got %s", test.Name, test.Expected, actual)
+			}
+		})
+	}
+}
