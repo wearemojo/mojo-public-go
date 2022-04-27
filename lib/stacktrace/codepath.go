0b80beb299f4143b67f6afcde7115cac32280f83
Laura Miller <git@duffle.one>
2022-04-27 11:56:18 +0100

    Always provide the skip

--- lib/stacktrace/codepath.go
+++ lib/stacktrace/codepath.go
@@ -5,12 +5,13 @@ import (
 	"runtime"
 )
 
-func GetCallerCodePath(fallback string) string {
-	return GetCallerCodePathWithSkip(1, fallback)
-}
-
-func GetCallerCodePathWithSkip(skip int, fallback string) string {
-	_, file, line, ok := runtime.Caller(1 + skip)
+// GetCallerCodePath returns the caller path and line number.
+//
+// The argument skip is the number of stack frames to skip before identifying
+// the frame to use, with 0 identifying the frame for GetCallerCodePath itself
+// and 1 identifying the caller of GetCallerCodePath.
+func GetCallerCodePath(skip int, fallback string) string {
+	_, file, line, ok := runtime.Caller(skip)
 	if !ok || file == "" {
 		return fallback
 	}
--- lib/stacktrace/codepath_test.go
+++ lib/stacktrace/codepath_test.go
@@ -10,7 +10,7 @@ import (
 func TestGetCallerCodePath(t *testing.T) {
 	is := is.New(t)
 
-	codePath := GetCallerCodePath("")
+	codePath := GetCallerCodePath(1, "")
 
 	is.True(strings.HasSuffix(codePath, "/lib/stacktrace/codepath_test.go:13"))
 }
